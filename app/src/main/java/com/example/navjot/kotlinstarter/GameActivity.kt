3e73bd5e4822f59126af044cbd45492c7ce368ce
NJ-Developer <navsingh191@gmail.com>
2018-07-09 10:52:27 -0400

    Initial commit!! kotlin android czyDevps

---
+++ app/src/main/java/com/example/navjot/kotlinstarter/GameActivity.kt
@@ -0,0 +1,27 @@
+package com.example.navjot.kotlinstarter
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+    private fun resetGame(){
+
+    }
+
+    private fun startGame(){
+
+    }
+
+    private fun  endGame(){
+
+    }
+
+    private fun incrementScore(){
+
+    }
+
+}
