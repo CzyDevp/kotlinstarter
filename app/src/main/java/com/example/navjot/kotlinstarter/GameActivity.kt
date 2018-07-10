048e00cdf0a88b055a0d8116f136105ba4ea66e1
NJ-Developer <navsingh191@gmail.com>
2018-07-10 11:02:07 -0400

    variables and views references added!! increment score fun implemented

--- app/src/main/java/com/example/navjot/kotlinstarter/GameActivity.kt
+++ app/src/main/java/com/example/navjot/kotlinstarter/GameActivity.kt
@@ -2,12 +2,24 @@ package com.example.navjot.kotlinstarter
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.TextView
+
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var gameScore : TextView
+    private lateinit var gameTime: TextView
+    private lateinit var hitMe: Button
+    internal var score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        gameScore = findViewById(R.id.game_score)
+        gameTime = findViewById(R.id.time)
+        hitMe = findViewById(R.id.hit_me)
+        hitMe.setOnClickListener{_->incrementScore()}
     }
+
     private fun resetGame(){
 
     }
@@ -21,7 +33,9 @@ class GameActivity : AppCompatActivity() {
     }
 
     private fun incrementScore(){
-
+      score++
+        var newScore = "Your score: " + Integer.toString(score)
+        gameScore.text = newScore
     }
 
 }
